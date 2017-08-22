54dd8767b0ee55b3ab3d2016294a2121b8a0fa4f
sivashankerN <shankar@vlabs.ac.in>
2017-08-22 15:05:50 +0530

    Update ldap configuration files

--- projects/backups/backup-config-files/ldap.vlabs.ac.in/root/ldap_database/ldap_database_backup.sh
+++ projects/backups/backup-config-files/ldap.vlabs.ac.in/root/ldap_database/ldap_database_backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd ldap_database/
-slapcat -l ldap_database-$(date +'%Y-%m-%d').ldif -f /etc/openldap/slapd.conf
+cd ~/ldap_database/
+/usr/sbin/slapcat -l ldap_database-$(date +'%Y-%m-%d').ldif -f /etc/openldap/slapd.conf
 cd ..
